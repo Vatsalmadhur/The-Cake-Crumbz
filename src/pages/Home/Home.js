0f9ccb7372a37edc41ff4874d3a1530ba0fd0ff0
Somiya <somiyakumari2353@gmail.com>
2024-03-16 21:04:49 +0530

    updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Section1 from "./Section1"
 import Section2 from "./Section2"
 import Layout from '../../components/Layouts/Layout';
 import "../../styles/HomeStyle.css"
+import Section3 from './Section3';
 function Home() {
   return (
     <>
@@ -11,6 +12,8 @@ function Home() {
     <Section1/>
     {/*Home Section About */}
     <Section2/>
+    {/*Home Section Menu*/}
+    <Section3/>
     </Layout>
     </>
   )
